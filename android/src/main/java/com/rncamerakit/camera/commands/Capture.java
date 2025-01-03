9f6e67a008412a5e7f1fb9f12128fd69db8b61ac
jchenTPlink <jonathan.chen@tp-link.com>
2025-01-02 17:59:52 -0800

    12

--- android/src/main/java/com/rncamerakit/camera/commands/Capture.java
+++ android/src/main/java/com/rncamerakit/camera/commands/Capture.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.TotalCaptureResult;
 import android.media.Image;
