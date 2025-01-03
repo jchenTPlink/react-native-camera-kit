2b3bdb7697d2eacc5d34ffaa94a5155f965f2cf6
jchenTPlink <jonathan.chen@tp-link.com>
2025-01-02 16:37:56 -0800

    commit5

--- android/src/main/java/com/rncamerakit/camera/CameraView.java
+++ android/src/main/java/com/rncamerakit/camera/CameraView.java
@@ -33,23 +33,47 @@ public class CameraView extends FrameLayout implements SurfaceHolder.Callback {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        int actualPreviewWidth = getResources().getDisplayMetrics().widthPixels;
-        int actualPreviewHeight = getResources().getDisplayMetrics().heightPixels;
+        int actualPreviewWidth = getWidth();
+        int actualPreviewHeight = getHeight();
 
-        // Maintain the original aspect ratio
-        float aspectRatio = (float) actualPreviewWidth / actualPreviewHeight;
-        int previewWidth = actualPreviewWidth;
-        int previewHeight = (int) (actualPreviewWidth / aspectRatio);
+        // Get the camera's aspect ratio (replace this with the actual camera aspect ratio)
+        float cameraAspectRatio = 16f / 9f; // Replace with your camera's actual aspect ratio
+        int previewHeight;
+        int previewWidth;
 
-        // Center the preview if there are black bars
-        int topOffset = (actualPreviewHeight - previewHeight) / 2;
+        // Calculate dimensions to maintain aspect ratio
+        if (actualPreviewWidth / (float) actualPreviewHeight > cameraAspectRatio) {
+            // Screen is wider than the camera aspect ratio
+            previewWidth = actualPreviewWidth;
+            previewHeight = (int) (actualPreviewWidth / cameraAspectRatio);
+        } else {
+            // Screen is taller than the camera aspect ratio
+            previewHeight = actualPreviewHeight;
+            previewWidth = (int) (actualPreviewHeight * cameraAspectRatio);
+        }
 
-        surface.layout(0, topOffset, previewWidth, topOffset + previewHeight);
+        // Center the preview if it doesn't fill the entire screen
+        int horizontalOffset = (actualPreviewWidth - previewWidth) / 2;
+        int verticalOffset = (actualPreviewHeight - previewHeight) / 2;
+
+        // Layout the surface view to match the calculated dimensions
+        surface.layout(
+            horizontalOffset,
+            verticalOffset,
+            horizontalOffset + previewWidth,
+            verticalOffset + previewHeight
+        );
 
         if (barcodeFrame != null) {
-            ((View) barcodeFrame).layout(0, topOffset, previewWidth, topOffset + previewHeight);
+            ((View) barcodeFrame).layout(
+                horizontalOffset,
+                verticalOffset,
+                horizontalOffset + previewWidth,
+                verticalOffset + previewHeight
+            );
         }
     }
+
 
 
     @Override
